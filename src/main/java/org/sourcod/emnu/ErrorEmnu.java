2dc530b5db08b16b361088fde58f4a8db2b7bb0d
willeam <zhaochunjie@sourcod.com>
2016-09-01 17:29:12 +0800

    "2016-09-01 17:29:21"

---
+++ src/main/java/org/sourcod/emnu/ErrorEmnu.java
@@ -0,0 +1,35 @@
+package org.sourcod.emnu;
+
+/**
+ * ErrorEmnu
+ *
+ * @author willeam
+ * @time 16-8-31 下午8:04
+ */
+public enum ErrorEmnu {
+
+    ERROR_PROPERTY_FORMAT,
+    ERROR_PROPERTY_VALUE,
+    ERROR_PROPERTY_PARSE,
+    ERROR_PROPERTY_NULL,
+    ERROR_PROPERTY_EXCEPTION,
+    ERROR_PARAMETER_VALUE,
+    ERROR_PARAMETER_FORMAT,
+    ERROR_PARAMETER_PARSE,
+    ERROR_PARAMETER_NULL,
+    ERROR_PARAMETER_EXCEPTION;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
